d93e26d613f25dc30da8325c80a6e1101a3cc086
intrsivakumar <53820683+intrsivakumar@users.noreply.github.com>
2023-06-20 20:42:20 +0530

    Add files via upload
    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+var bodyParser = require('body-parser');
+const { getPersona } = require('./persona');
+
+const app = express()
+app.use(bodyParser.json());
+const port = 8000
+
+app.get('/', (req, res) => {
+    res.json({ user: 'geek' });
+})
+app.post('/login', (req, res) => {
+    console.log("request: ", req.body);
+    res.json(getPersona().professional);
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
