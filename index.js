c7df995a8723561d403ca17c7a5da635bad2afc1
intrsivakumar <53820683+intrsivakumar@users.noreply.github.com>
2023-07-18 20:02:44 +0530

    Update index.js
    Updated for json based on email

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const { getPersona } = require('./persona');
 
 const app = express()
 app.use(bodyParser.json());
+app.use(express.static('public'))
 const port = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
     res.json({ user: 'geek' });
 })
 app.post('/login', (req, res) => {
-    console.log("request: ", req.body);
-    res.json(getPersona());
+    console.log("request: ", req.body?.email);
+    res.json(getPersona(req.body?.email));
 })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
